5556683b567a9703629834c0456c11bf9cec413c
elhs25 <hsortiz05@gmail.com>
2021-03-27 11:44:52 -0500

    🌟 Added UNLICENSED license to example contract

--- src/contracts/ExampleContract.sol
+++ src/contracts/ExampleContract.sol
@@ -1,3 +1,5 @@
+// Warning: Provide a license identifier before deploy
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.5.0 <0.9.0;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
@@ -5,5 +7,5 @@ import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 contract ExampleContract is ERC721 {
      
     // Example constructor 
-    constructor() ERC721("ExampleContract", "EXAMC") public {}
+    constructor() ERC721("ExampleContract", "EXAMC") {}
 }
